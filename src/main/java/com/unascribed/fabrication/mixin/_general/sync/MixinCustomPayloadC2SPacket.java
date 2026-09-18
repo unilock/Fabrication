078efc4d4c064fbe4f08ab829e295bd021995ae0
unilock <katherines1337@outlook.com>
2026-09-18 05:08:19 -0400

    make custom payload codec injection more compatible?

--- src/main/java/com/unascribed/fabrication/mixin/_general/sync/MixinCustomPayloadC2SPacket.java
+++ src/main/java/com/unascribed/fabrication/mixin/_general/sync/MixinCustomPayloadC2SPacket.java
@@ -1,23 +1,24 @@
 package com.unascribed.fabrication.mixin._general.sync;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.unascribed.fabrication.util.ByteBufCustomPayload;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.network.packet.c2s.common.CustomPayloadC2SPacket;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(CustomPayloadC2SPacket.class)
 public class MixinCustomPayloadC2SPacket {
 
-	@ModifyArg(method="<clinit>()V", at=@At(value="INVOKE", target="Lnet/minecraft/network/packet/CustomPayload;createCodec(Lnet/minecraft/network/packet/CustomPayload$CodecFactory;Ljava/util/List;)Lnet/minecraft/network/codec/PacketCodec;"))
-	private static CustomPayload.CodecFactory<PacketByteBuf> oldPayload(CustomPayload.CodecFactory<PacketByteBuf> codecFactory){
-		return id -> {
-			if ("fabrication".equals(id.getNamespace())) {
-				return ByteBufCustomPayload.CODEC;
-			}
-			return codecFactory.create(id);
-		};
+	@WrapOperation(method="method_56475", at=@At(value="INVOKE", target="Lnet/minecraft/network/packet/UnknownCustomPayload;createCodec(Lnet/minecraft/util/Identifier;I)Lnet/minecraft/network/codec/PacketCodec;"))
+	private static PacketCodec<PacketByteBuf, ? extends CustomPayload> oldPayload(Identifier id, int maxBytes, Operation<PacketCodec<PacketByteBuf, ? extends CustomPayload>> original){
+		if ("fabrication".equals(id.getNamespace())) {
+			return ByteBufCustomPayload.CODEC;
+		}
+		return original.call(id, maxBytes);
 	}
 }
--- src/main/java/com/unascribed/fabrication/mixin/_general/sync/MixinCustomPayloadS2CPacket.java
+++ src/main/java/com/unascribed/fabrication/mixin/_general/sync/MixinCustomPayloadS2CPacket.java
@@ -1,23 +1,32 @@
 package com.unascribed.fabrication.mixin._general.sync;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.unascribed.fabrication.util.ByteBufCustomPayload;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.codec.PacketCodec;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.network.packet.s2c.common.CustomPayloadS2CPacket;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(CustomPayloadS2CPacket.class)
 public class MixinCustomPayloadS2CPacket {
 
-	@ModifyArg(method="<clinit>()V", at=@At(value="INVOKE", target="Lnet/minecraft/network/packet/CustomPayload;createCodec(Lnet/minecraft/network/packet/CustomPayload$CodecFactory;Ljava/util/List;)Lnet/minecraft/network/codec/PacketCodec;"))
-	private static CustomPayload.CodecFactory<PacketByteBuf> oldPayload(CustomPayload.CodecFactory<PacketByteBuf> codecFactory){
-		return id -> {
-			if ("fabrication".equals(id.getNamespace())) {
-				return ByteBufCustomPayload.CODEC;
-			}
-			return codecFactory.create(id);
-		};
+	@WrapOperation(method="method_56460", at=@At(value="INVOKE", target="Lnet/minecraft/network/packet/UnknownCustomPayload;createCodec(Lnet/minecraft/util/Identifier;I)Lnet/minecraft/network/codec/PacketCodec;"))
+	private static PacketCodec<PacketByteBuf, ? extends CustomPayload> oldPayloadConfiguration(Identifier id, int maxBytes, Operation<PacketCodec<PacketByteBuf, ? extends CustomPayload>> original){
+		if ("fabrication".equals(id.getNamespace())) {
+			return ByteBufCustomPayload.CODEC;
+		}
+		return original.call(id, maxBytes);
+	}
+
+	@WrapOperation(method="method_56461", at=@At(value="INVOKE", target="Lnet/minecraft/network/packet/UnknownCustomPayload;createCodec(Lnet/minecraft/util/Identifier;I)Lnet/minecraft/network/codec/PacketCodec;"))
+	private static PacketCodec<PacketByteBuf, ? extends CustomPayload> oldPayloadPlay(Identifier id, int maxBytes, Operation<PacketCodec<PacketByteBuf, ? extends CustomPayload>> original){
+		if ("fabrication".equals(id.getNamespace())) {
+			return ByteBufCustomPayload.CODEC;
+		}
+		return original.call(id, maxBytes);
 	}
 }
